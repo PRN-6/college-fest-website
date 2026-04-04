1f6cf7e1736df1c041edeae02d0c80b0d37c6730
prinsonnazareth2@gmail.com <prinsonnazareth2@gmail.com>
2026-04-04 09:37:23 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Scene from './components/Scene'
 import Background from './components/Background'
 import Header from './components/Header'
 
-
-
 const App = () => {
 
   return (
@@ -16,12 +14,8 @@ const App = () => {
       <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-screen">
         <Scene/>
       </div>
-
     </div>
-
   )
-
-
 }
-
 export default App
+
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -9,20 +9,17 @@ const Model = () => {
   const group = useRef()
   const { scene , animations} = useGLTF('/models/laptop-proper.glb')
   const { actions } = useAnimations(animations, scene)
-
-
   // play the animation when the component mounts
+
   useEffect(() => {
     const action = actions[Object.keys(actions)[0]]
     action.play()
     action.paused = true
-
     // set the initial rotation
     gsap.set(group.current.rotation, {
       x: Math.PI / 10,
       y: Math.PI / 6,
     })
-
 
     // scroll trigger to control the animation
     ScrollTrigger.create({
@@ -34,12 +31,10 @@ const Model = () => {
         const progress = self.progress;
         action.time = progress * action.getClip().duration;
 
-
         // rotate the model
         group.current.rotation.x = gsap.utils.interpolate(Math.PI / 10, 0, progress);
         group.current.rotation.y = gsap.utils.interpolate(Math.PI / 6, 0, progress);
         group.current.rotation.z = gsap.utils.interpolate(0, 0, progress);
-        
         group.current.position.x = gsap.utils.interpolate(0, 0, progress);
         group.current.position.y = gsap.utils.interpolate(0, 0, progress);
         group.current.position.z = gsap.utils.interpolate(0, 2 - (progress * 2), progress);
@@ -47,7 +42,6 @@ const Model = () => {
       }
     })
   },[actions])
-
 
   return (
     <primitive 
