422e9f89d371d03e52bc0bc138fffa9f8f444fc6
prinsonnazareth2@gmail.com <prinsonnazareth2@gmail.com>
2026-04-06 23:32:53 +0530

    added bottom navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Scene from './components/Scene'
 import Background from './components/Background'
 import Header from './components/Header'
 import Timeline from './components/Timeline'
-
+import FooterNav from './components/FooterNav'
+import './styles/hide-scrollbar.css'
 
 const App = () => {
 
   return (
-    <div className='w-full h-[500vh] relative'>
+    <div className='w-full h-[500vh] relative overflow-hidden'>
       <Background /> 
       <Header/>  
       <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-screen">
@@ -15,7 +16,7 @@ const App = () => {
       </div>
       
       <Timeline />
-
+      <FooterNav />
     </div>
   )
 }
